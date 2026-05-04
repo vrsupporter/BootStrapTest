41148559162d77b4316fa2084b60a72222d0073c
vrsupporter <vrsup.pnm@outlook.com>
2026-05-04 12:00:27 +0100

    fixed navbar interactions

--- java.js
+++ java.js
@@ -1,10 +1,18 @@
+
 function ToggleClass(id, classToToggle) {
     document.getElementById(id).classList.toggle(classToToggle)
 }
 
 function ToggleClassIfNoHoverSupport(id, classToToggle) {
-    const hoverSupport = window.matchMedia('(hover: hover)').matches
-    if (!hoverSupport) {
+    if (window.matchMedia('(hover: none)').matches) {
         ToggleClass(id, classToToggle);
     }
+}
+
+function RemoveClickOpenedClasses() {
+    const idsToAffect = ["projectDropdownHolder"];
+    const attributeToRemove = "clickOpened";
+    idsToAffect.forEach(id => {
+        document.getElementById(id).classList.remove(attributeToRemove);
+    });
 }
